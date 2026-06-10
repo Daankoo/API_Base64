706d03b958f8ff7c4c2ed29b26d2d0a61425b5d2
Danko <bogdbutk-ipt28@lll.kpi.ua>
2026-06-10 13:24:53 +0300

    Оновлення структури за закінчення кодеру

--- main.cpp
+++ main.cpp
@@ -1,77 +1,122 @@
-/*
-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 + \
-*/
-
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <bitset>
 
 using namespace std;
 
-int main() {
-	
-	string Base64_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-	
-	//string NameFile;
-	//cout << "Enter name file";
-	//cin >> NameFile;
+const string BASE64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+const int LINE_LENGTH = 76;
 
-	ifstream ReadFile("D:\\Study\\University\\API\\Вase64_fail\\read.txt", ios::binary);
-	
-	if (!ReadFile) {
-		cout << "File is not open";
-		return -1; // Завершення програми з помилкою
-	}
-
-	ofstream WriteFile("D:\\Study\\University\\API\\Вase64_fail\\write.txt");
-
-	if (!WriteFile) {
-		cout << "File is not found";
-		return -1;
-	}
-
-	unsigned char byte[3];
-	unsigned char ind1, ind2, ind3, ind4;
-
-	while (true) {	
-
-		byte[0] = 0;
-		byte[1] = 0;
-		byte[2] = 0;
-
-		ReadFile.read((char*)byte, 3);
-		int ReadSize = ReadFile.gcount();
-		
-		if (ReadSize == 0) {
-			break;
-		}
-
-		if (ReadSize == 3) {
-			ind1 = byte[0] >> 2;
-			ind2 = ((byte[0] & 3) << 4) ^ (byte[1] >> 4);
-			ind3 = ((byte[1] & 15) << 2) ^ (byte[2] >> 6);
-			ind4 = byte[2] & 63;
-
-			WriteFile << Base64_char[ind1] << Base64_char[ind2] << Base64_char[ind3] << Base64_char[ind4];
-		}
-		else if (ReadSize == 2) {
-			ind1 = byte[0] >> 2;
-			ind2 = ((byte[0] & 3) << 4) ^ (byte[1] >> 4);
-			ind3 = ((byte[1] & 15) << 2);
-			
-			WriteFile << Base64_char[ind1] << Base64_char[ind2] << Base64_char[ind3] << "=";
-		}
-		else {
-			ind1 = byte[0] >> 2;
-			ind2 = ((byte[0] & 3) << 4);
-
-			WriteFile << Base64_char[ind1] << Base64_char[ind2] << "==";
-		}
-	}
-
-	WriteFile.close();
-
-	return 0;
+// Записує символ у файл, автоматично розбиваючи на рядки по 76 символів
+void writeChar(ofstream& file, char c, int& linePos) {
+    if (linePos == LINE_LENGTH) {
+        file << '\n';
+        linePos = 0;
+    }
+    file << c;
+    linePos++;
 }
 
+void encode(const string& inputName, const string& outputName, const string& comment) {
+    ifstream readFile(inputName, ios::binary);
+    if (!readFile) {
+        cout << "Error: could not open file \"" << inputName << "\"\n";
+        return;
+    }
+
+    ofstream writeFile(outputName);
+    if (!writeFile) {
+        cout << "Error: could not create file \"" << outputName << "\"\n";
+        return;
+    }
+
+    // Записуємо коментар якщо є
+    if (!comment.empty()) {
+        // Символ '-' + текст, не більше 76 символів загалом
+        string commentLine = "-" + comment;
+        if (commentLine.length() > LINE_LENGTH) {
+            commentLine = commentLine.substr(0, LINE_LENGTH);
+        }
+        writeFile << commentLine << '\n';
+    }
+
+    unsigned char bytes[3];
+    unsigned char ind1, ind2, ind3, ind4;
+    int linePos = 0;
+
+    while (true) {
+        bytes[0] = 0;
+        bytes[1] = 0;
+        bytes[2] = 0;
+
+        readFile.read((char*)bytes, 3);
+        int readSize = readFile.gcount();
+
+        if (readSize == 0) break;
+
+        if (readSize == 3) {
+            ind1 = bytes[0] >> 2;
+            ind2 = ((bytes[0] & 3) << 4) | (bytes[1] >> 4);
+            ind3 = ((bytes[1] & 15) << 2) | (bytes[2] >> 6);
+            ind4 = bytes[2] & 63;
+
+            writeChar(writeFile, BASE64_CHARS[ind1], linePos);
+            writeChar(writeFile, BASE64_CHARS[ind2], linePos);
+            writeChar(writeFile, BASE64_CHARS[ind3], linePos);
+            writeChar(writeFile, BASE64_CHARS[ind4], linePos);
+        }
+        else if (readSize == 2) {
+            ind1 = bytes[0] >> 2;
+            ind2 = ((bytes[0] & 3) << 4) | (bytes[1] >> 4);
+            ind3 = ((bytes[1] & 15) << 2);
+
+            writeChar(writeFile, BASE64_CHARS[ind1], linePos);
+            writeChar(writeFile, BASE64_CHARS[ind2], linePos);
+            writeChar(writeFile, BASE64_CHARS[ind3], linePos);
+            writeChar(writeFile, '=', linePos);
+        }
+        else { // readSize == 1
+            ind1 = bytes[0] >> 2;
+            ind2 = ((bytes[0] & 3) << 4);
+
+            writeChar(writeFile, BASE64_CHARS[ind1], linePos);
+            writeChar(writeFile, BASE64_CHARS[ind2], linePos);
+            writeChar(writeFile, '=', linePos);
+            writeChar(writeFile, '=', linePos);
+        }
+    }
+
+    // Завершальний перенос рядка якщо файл не порожній
+    if (linePos > 0) {
+        writeFile << '\n';
+    }
+
+    writeFile.close();
+    readFile.close();
+
+    cout << "Encoding complete. Result saved to \"" << outputName << "\"\n";
+}
+
+int main() {
+    string inputName, outputName, comment;
+
+    cout << "=== Base64 Encoder ===\n\n";
+
+    cout << "Enter input file name: ";
+    getline(cin, inputName);
+
+    cout << "Enter output file name (press Enter to skip): ";
+    getline(cin, outputName);
+
+    if (outputName.empty()) {
+        outputName = inputName + ".base64";
+        cout << "Output file: " << outputName << "\n";
+    }
+
+    cout << "Enter a comment for the file (press Enter to skip): ";
+    getline(cin, comment);
+
+    encode(inputName, outputName, comment);
+
+    return 0;
+}
